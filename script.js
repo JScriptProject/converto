68419d08ac6235cd354df3a13788740847efd807
Ravindra Kamble <write2ravi14@gmail.com>
2025-05-01 14:27:49 +0530

    ddd

--- script.js
+++ script.js
@@ -1,119 +1,82 @@
 
-const flagApi = 'https://flagsapi.com/US/shiny/64.png';
-const inputBox = document.querySelector("input"); 
-const btn = document.querySelector("button");
-const countryBox = document.querySelectorAll(".country-box");
-const flagOption = document.querySelectorAll("option");
-let countryList_country;
-let countryList_currency;
-let currencyFrom ="INR";
-let currencyTo ="INR";
 let countryFrom = "IN";
-let countryTo ="IN"
-
-const getInput=()=>{ 
-  if(!inputBox.value)
-  {
-    inputBox.value=1;
-  }
-  else if(inputBox.value<1)
-  {
-    inputBox.value=1;
-  }
-return inputBox.value;
-}
-
-const convertCurrency = async () =>{
-const BASE_URL = 'https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2025.4.23/v1/currencies/inr.json'
-const urlFrom = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2025.4.23/v1/currencies/${currencyFrom.toLowerCase()}.json`;
-
-let response = await fetch(urlFrom);
-let result = await response.json();
-console.log(result);
-let crf = currencyFrom.toLowerCase();
-let crt = currencyTo.toLowerCase();
-let exchangeRate = result[crf][crt];
-console.log(exchangeRate);
-console.log("below");
-const inputCurrency = getInput();
-console.log(inputCurrency)
-let convertedCurrency = (exchangeRate*inputCurrency).toFixed(1);
-document.querySelector(".converted-currency").innerText = `${inputCurrency} ${currencyFrom} = ${convertedCurrency} ${currencyTo}`;
-document.querySelector(".exchg-value").innerText = `${exchangeRate.toFixed(2)}`;
-document.querySelector(".exchg-currency").innerText = currencyFrom;
-}
+let countryTo = "US";
+let currencyFrom = "INR";
+let currencyTo = "USD";
 
 
+// Load the data into the select items
 
-btn.addEventListener("click", (e)=>{
-  e.preventDefault();
- const inputCurrency = getInput();
- console.log(inputCurrency);
-//  inputBox.value = "";
-  console.log(currencyFrom);
-  console.log(currencyTo);
-  
-  convertCurrency();
-})
+const dropDown = document.querySelectorAll("select");
 
-
-
-
-document.querySelectorAll("#select").forEach((select)=>{
-    select.addEventListener("change", async (e)=>{
-        console.log(e.target.selectedOptions[0].getAttribute("name"));
-        let selectedCountryCode = e.target.selectedOptions[0].getAttribute("name");
-        let imgUrl = `https://flagsapi.com/${selectedCountryCode}/shiny/64.png`;
-        console.log(imgUrl);
-        select.parentElement.querySelector(".flag").querySelector("img").src = imgUrl;
-        console.log(select.parentElement.querySelector(".flag").querySelector("img"));
-        console.log(select);
-        
-        // get the value of the currancy 
-        if(select.getAttribute("name") =="from-country")
-        { 
-          currencyFrom = e.target.value;
-          console.log("from value =", currencyFrom);
-          // console.log((select.parentElement).parentElement.querySelector("h5").textContent());
-          select.parentElement.parentElement.querySelector("h5").innerText = e.target.selectedOptions[0].getAttribute("data-name");
-
-
-        }
-        else if(select.getAttribute("name") =="to-country")
+window.addEventListener("load", function(){
+    dropDown.forEach((select)=>{
+        for(let i=0; i<Object.keys(countryList).length; i++)
         {
-          currencyTo = e.target.value;
-          console.log("to value =", currencyTo)
-          select.parentElement.parentElement.querySelector("h5").innerText = e.target.selectedOptions[0].getAttribute("data-name");
+            // Create new DOM element 'options'
+            const newElement = document.createElement("option");
+            newElement.value = Object.keys(countryList)[i];
+            newElement.textContent = Object.keys(countryList)[i];
+            newElement.setAttribute("name", Object.values(countryList)[i][0]);
+            newElement.setAttribute("cname", Object.values(countryList)[i][1]);
+            select.append(newElement);
+
+            // Logic to set teh default options in dropdown
+            if(newElement.value ==="INR" && select.getAttribute("name")=="currency-from")
+            {
+                newElement.selected = true;
+            }
+             if(newElement.value ==="USD" && select.getAttribute("name")==="currency-to")
+            {
+                newElement.selected = true;
+            }
         }
-        
     })
 })
 
+// Select the values from dropdown on click and update the flag
 
-window.addEventListener("load", ()=>{
-  let select= document.querySelectorAll("#select");
-  for(let i=0;i<select.length;i++)
-  {
-     countryList_currency =  Object.keys(countryList);
-     countryList_country =  Object.values(countryList);
-
-
-    for(let j=0; j<countryList_country.length;j++)
-    { 
-        let newOption = document.createElement("option");
-        newOption.value = countryList_currency[j];
-        newOption.textContent = countryList_currency[j];
-        
-        newOption.setAttribute("name", `${countryList_country[j][0]}`);
-        newOption.setAttribute("data-name", `${countryList_country[j][1]}`)
-        console.log();
-        select[i].append(newOption);
+dropDown.forEach((select)=>{
+   select.addEventListener("click", (e)=>{
+    console.log(e.target);
+    if(e.target.getAttribute("name")==="currency-from")
+    {
+        currencyFrom =  (e.target.value);
+        countryFrom = (e.target.options[e.target.selectedIndex].getAttribute("name"));
+        const cNameFrom = e.target.options[e.target.selectedIndex].getAttribute("cname");
+        select.parentElement.querySelector("img").src = `https://flagsapi.com/${countryFrom}/flat/64.png`;
+        (select.parentElement).parentElement.querySelector("h3").textContent = cNameFrom; 
     }
-  }
- document.querySelectorAll("option").forEach((option)=>{
-  if(option.getAttribute("name") === "IN")
-  {
-    option.selected = true;
-  }
- })
+    if(e.target.getAttribute("name")==="currency-to")
+    {
+        currencyTo =  (e.target.value);
+        countryTo = e.target.options[e.target.selectedIndex].getAttribute("name");
+        const cNameTo = e.target.options[e.target.selectedIndex].getAttribute("cname");
+        select.parentElement.querySelector("img").src = `https://flagsapi.com/${countryTo}/flat/64.png`;
+        (select.parentElement).parentElement.querySelector("h3").textContent = cNameTo; 
+    }
+  })
+  
 })
+
+// Get Exchange Rate and update to display
+
+const runAPI = async() => {
+
+    const result = await (await fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2025.4.23/v1/currencies/${currencyFrom.toLowerCase()}.json`)).json();
+    console.log(currencyFrom);
+    console.log(currencyTo);
+    console.log(result);
+    const exRate = (result[currencyFrom.toLowerCase()][currencyTo.toLowerCase()]);
+    const inputVal = document.querySelector("input").value;
+    // display output
+    document.querySelector(".output").innerText = `${inputVal} ${currencyFrom} = ${(exRate * inputVal).toFixed(2)} ${currencyTo}`; 
+    document.querySelector(".ex-rate-show").textContent = `${exRate.toFixed(2)}`
+    
+}
+
+document.querySelector("button").addEventListener("click", runAPI);
+
+
+
+
